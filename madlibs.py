42110056e71a2539702fe8208c815f5b8ed8c38f
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-09-03 09:15:41 -0700

    Add comments

--- madlibs.py
+++ madlibs.py
@@ -1,18 +1,16 @@
 import random
 
-#4
 adjectives = list()
-#2
 nouns = list()
-#4
 verbs = list()
-#2
 verb_ing = list()
-#2
 integers = list()
-#1
+# Article of clothing
 other = list()
 
+#
+# FUNCTIONS TO DRAW RANDOM WORDS FROM EACH LIST
+#
 def random_adj():
     item = random.choice(adjectives)
     adjectives.remove(item)
@@ -29,13 +27,14 @@ def random_verb():
     return item
 
 def random_verb_ing():
-    return random.choice(verbs)
+    return random.choice(verb_ing).capitalize()
 
 def random_int():
     item = random.choice(integers)
     integers.remove(item)
     return item
 
+# GET WORDS FROM USER
 def user_input(prompt):
     user_input = input(prompt)
     return user_input
@@ -47,7 +46,7 @@ def list_all_items(list):
     for item in list:
         print("{} {}".format(index, item))
         index += 1
-
+# TEST
 def test():
     list_all_items(adjectives)
     list_all_items(nouns)
@@ -68,15 +67,17 @@ def test():
     print('\n')
     list_all_items(verbs)
 
-
 def print_story():
-    print("Mondays are always {}. I {} my alarm {} times until I was officially late. I {} out of bed, {} on clothes, {} my teeth, and was out of the dorm building in {} minutes. (Verb(ing)) down the hills of San Francisco, I found myself completely out of breath. I run inside Make School and take off my (article of clothing), exposing mismatched socks. I curse the Gods for a second and focus my attention back to getting to my Huddle before it ends. I (verb) up the steps, not forgetting to trip a couple times, and dart for the Great Hall. Before getting there, though, I realize the (noun) is pretty empty. & now that I think about it, I didn’t see any other shoes down in the (noun). I get into the Great Hall and no one is there. I look at my calendar and (verb) myself. It’s Labor Day.".))
+    print("Mondays are always {}. I {} my alarm {} times until I am officially late. I {} out of bed, {} on clothes, {} my teeth, and was out of the dorm building in {} minutes. {} down the hills of San Francisco, I found myself completely out of breath. I run inside Make School and take off my {}, exposing mismatched socks. I curse the Gods for a second and focus my attention back to getting to my Huddle before it ends. I {} up the steps, not forgetting to trip a couple times, and dart for the Great Hall. Before getting there, though, I realize the {} is pretty empty. & now that I think about it, I didn’t see any other shoes down in the {}. I get into the Great Hall and no one is there. I look at my calendar and {} myself. It’s Labor Day.".format(random_adj(), random_verb(), random_int(), random_verb(), random_verb(), random_verb(), random_int(), random_verb_ing(), other[0], random_verb(), random_noun(), random_noun(), random_verb()))
 
+#
+# GET INPUT AND STORE IN EACH LIST
+#
 for i in range(1, 5):
     adj = user_input("Input adjective: ")
     adjectives.append(adj)
 
-for i in range(1, 5):
+for i in range(1, 7):
     verb = user_input("Input verb: ")
     verbs.append(verb)
 
@@ -95,4 +96,5 @@ for i in range(1, 3):
 clothing = user_input("Input article of clothing: ")
 other.append(clothing)
 
-test()
+#test()
+print_story()
