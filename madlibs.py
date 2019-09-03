dc088705076e7c31d7f8c20ef676097aafd1bef9
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-09-03 12:23:20 -0700

    Add formatting functions

--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,6 @@
 import random
+from colorama import Fore, Back, Style
+import textwrap
 
 adjectives = list()
 nouns = list()
@@ -7,6 +9,7 @@ verb_ing = list()
 integers = list()
 # Article of clothing
 article_of_clothing = ""
+story = ""
 
 #
 # FUNCTIONS TO DRAW RANDOM WORDS FROM EACH LIST
@@ -14,24 +17,31 @@ article_of_clothing = ""
 def random_adj():
     item = random.choice(adjectives)
     adjectives.remove(item)
+    item = color_text(item)
     return item
 
 def random_noun():
     item = random.choice(nouns)
     nouns.remove(item)
+    item = color_text(item)
     return item
 
 def random_verb():
     item = random.choice(verbs)
     verbs.remove(item)
+    item = color_text(item)
     return item
 
 def random_verb_ing():
-    return random.choice(verb_ing).capitalize()
+    # This word starts a sentence so I'm capitalizing the first letter
+    item = random.choice(verb_ing).capitalize()
+    item = color_text(item)
+    return item
 
 def random_int():
     item = random.choice(integers)
     integers.remove(item)
+    item = color_text(item)
     return item
 
 # GET WORDS FROM USER
@@ -46,6 +56,10 @@ def list_all_items(list):
     for item in list:
         print("{} {}".format(index, item))
         index += 1
+
+def color_text(word):
+    return Fore.MAGENTA + word + Style.RESET_ALL
+
 # TEST
 def test():
     list_all_items(adjectives)
@@ -54,21 +68,13 @@ def test():
     list_all_items(integers)
     print(article_of_clothing)
 
-    list_all_items(adjectives)
-    list_all_items(verbs)
-
     print('\n')
-
     print_story()
 
-    print('\n')
-
-    list_all_items(adjectives)
-    print('\n')
-    list_all_items(verbs)
-
 def print_story():
-    print("Mondays are always {}. I {} my alarm {} times until I am officially late. I {} out of bed, {} on clothes, {} my teeth, and was out of the dorm building in {} minutes. {} down the hills of San Francisco, I found myself completely out of breath. I run inside Make School and take off my {}, exposing mismatched socks. I curse the Gods for a second and focus my attention on getting to my Huddle before it ends. I {} up the steps, not forgetting to trip a couple times, and dart for the Great Hall. Before getting there, though, I realize the {} is pretty empty. & now that I think about it, I didn’t see any other shoes down in the {}. I get into the Great Hall and no one is there. I look at my calendar and {} myself. It’s Labor Day.".format(random_adj(), random_verb(), random_int(), random_verb(), random_verb(), random_verb(), random_int(), random_verb_ing(), article_of_clothing, random_verb(), random_noun(), random_noun(), random_verb()))
+    print('\n')
+    print(textwrap.fill(story, 53))
+    print('\n')
 
 #
 # GET INPUT AND STORE IN EACH LIST
@@ -94,6 +100,8 @@ for i in range(1, 3):
     verb_ing.append(verb)
 
 article_of_clothing = user_input("Input article of clothing: ")
+
+story = "Mondays are always " + random_adj() + ". I " + random_verb() + " my alarm " + random_int() + " times until I am officially late. I " + random_verb() + " out of bed, " + random_verb() + " on clothes, " + random_verb() + " my teeth, and was out of the dorm building in " + random_int() + " minutes. " + random_verb_ing() + " down the hills of San Francisco, I found myself completely out of breath. I run inside Make School and take off my " + color_text(article_of_clothing) + ", exposing mismatched socks. I curse the Gods for a second and focus my attention on getting to my huddle before it ends. I " + random_verb() + " up the steps, not forgetting to trip a couple times, and dart for the Great Hall. Before getting there, though, I realize the " + random_noun() + " is pretty empty. & now that I think about it, I didn’t see any other shoes down in the " + random_noun() + ". I get into the Great Hall and no one is there. I " + random_verb() + " at my calendar and face-palm myself. It’s Labor Day."
 
 #test()
 print_story()
