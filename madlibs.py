320f81ce869936bbe09c7d6b77b6efc4092dc5a4
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-09-03 08:10:59 -0700

    Add story

---
+++ madlibs.py
@@ -0,0 +1,5 @@
+user_inputted_words = {nouns: [], verbs: [], adjectives: []}
+
+
+def print_story():
+    print("Mondays are always (adjective). I (verb) my alarm (integer) times until I was officially late. I (adjective) out of bed, (adjective) on clothes, (adjective) my teeth, and was out of the dorm building in (integer) minutes. (Verb (ing)) down the hills of San Francisco, I found myself completely out of breath. I run inside Make School and take off my (article of clothing), exposing mismatched socks. I curse the Gods for a second and focus my attention back to getting to my Huddle before it ends. I (verb) up the steps, not forgetting to trip a couple times, and dart for the Great Hall. Before getting there, though, I realize the (noun) is pretty empty. & now that I think about it, I didn’t see any other shoes down in the (noun). I get into the Great Hall and no one is there. I look at my calendar and (verb) myself. It’s Labor Day.".format())
